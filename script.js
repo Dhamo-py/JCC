1c7f7fa83bc14334b33e488b1ac66bf43f3d1583
Dhamo-py <dhamodharand08@gmail.com>
2025-04-09 19:07:50 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function whatsapp(phoneNumber) {
-    var message = encodeURIComponent("Hello, I would like to contact you.");
-    var url = "https://wa.me/" + phoneNumber.replace(/\D/g, '') + "?text=" + message;
-    window.open(url, "_blank");
-}
-function phone() {
-    var PhoneNumber = "9841536867";
-    window.location.href = "tel:" + 9841536867;
-}
