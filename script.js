bdc3a2c87c0e27d773a63e40773800ff206c36b7
Dhamo-py <dhamodharand08@gmail.com>
2025-04-13 19:58:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function whatsapp(phoneNumber) {
-    var message = encodeURIComponent("Hello, I would like to contact you.");
+    var message = encodeURIComponent("Hello! I visited your website and I'm interested in learning more about your courses. Could you please arrange a callback at your earliest convenience? Thank you!");
     var url = "https://wa.me/" + phoneNumber.replace(/\D/g, '') + "?text=" + message;
     window.open(url, "_blank");
 }
 function phone() {
-    var PhoneNumber = "9841536867";
-    window.location.href = "tel:" + 9841536867;
+    var PhoneNumber = "9578113101";
+    window.location.href = "tel:" + 9578113101;
 }
+
